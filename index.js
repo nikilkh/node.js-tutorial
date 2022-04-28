a3704d3b170200e142ef8d2e844b84b16383c8ff
Nikil K H <nikilkh241295@gmail.com>
2022-04-29 02:25:11 +0530

    first

--- index.js
+++ index.js
@@ -166,9 +166,7 @@ const http = require('http');
 const fs = require('fs');
 
 
-// fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+
 let commentarray = [];
 for(let i=1; i<=2; i++) {
     fetch(`https://jsonplaceholder.typicode.com/posts/${i}/comments`)
